7d03960347da5db00050990b983b7e7f741bc796
Sungyoon <sungyoonkim402@gmail.com>
2016-11-01 06:34:07 -0400

    Add close button image and remove bootstrap popover

--- example.js
+++
@@ -1,8 +0,0 @@
-angular.module('ui.bootstrap.demo', ['ngAnimate', 'ngSanitize', 'ui.bootstrap']);
-angular.module('ui.bootstrap.demo').controller('PopoverDemoCtrl', function ($scope, $sce) {
-  $scope.dynamicPopover = {
-    content: 'Hello, World!',
-    templateUrl: 'myPopoverTemplate.html',
-    title: 'Title'
-  };
-});
